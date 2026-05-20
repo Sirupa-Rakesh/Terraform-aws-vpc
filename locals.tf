62f398a455a634651bc302480b8d3a999db6dcae
Sirupa-Rakesh <siruparakeshrakhi@gmail.com>
2026-05-20 15:41:22 +0530

    actions

--- locals.tf
+++ locals.tf
@@ -1,36 +1,23 @@
-// ...existing code...
 locals {
-  common_tags = {
-    project     = var.project
-    environment = var.environment
-    terraform   = "true"
-  }
-
-  vpc_final_tags = merge(
-    local.common_tags,
-    {
-      Name = "${var.project}-${var.environment}"
-    },
-    var.vpc_tags
-  )
-
-  igw_final_tags = merge(
-    local.common_tags,
-    {
-       Name = "${var.project}-${var.environment}"
-    },
-    var.igw_tags
-  )
+    common_tags = {
+        Project = var.project
+        Environment = var.environment
+        Terraform = "true"
+    }
+    vpc_final_tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}"
+        },
+        var.vpc_tags
+    )
+    igw_final_tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}"
+        },
+        var.igw_tags
+    )
+    az_names = slice(data.aws_availability_zones.available.names, 0, 2)
+    
 }
-
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-locals {
-  az_name = slice(data.aws_availability_zones.available.names, 0, 2)
-}
-// ...existing code...
-
-
-
